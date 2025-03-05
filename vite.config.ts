b5de400b92feadc844bb915b862b3d01e7e0604d
Kudzai Mupita <67244898+kudzaimupita@users.noreply.github.com>
2025-03-05 23:32:19 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import dynamicImport from 'vite-plugin-dynamic-import';
-import path from "path";
-import react from "@vitejs/plugin-react";
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +9,11 @@ export default defineConfig({
     react({
       babel: {
         plugins: [
-          'babel-plugin-macros'
-        ]
-      }
+          // 'babel-plugin-macros'
+        ],
+      },
     }),
-    dynamicImport()
+    dynamicImport(),
   ],
   assetsInclude: ['**/*.md', '**/*.ttf', '**/*.otf', '**/*.woff', '**/*.woff2'],
   resolve: {
@@ -27,17 +27,6 @@ export default defineConfig({
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
       output: {
-        manualChunks: {
-          // Split vendor chunks
-          'vendor-react': ['react', 'react-dom'],
-          'vendor-mui': ['@mui/material', '@mui/icons-material'],
-          'vendor-ant': ['antd', '@ant-design/icons'],
-          'vendor-charts': ['recharts', '@ant-design/charts', 'apexcharts'],
-          'vendor-forms': ['react-hook-form', 'formik', 'yup', 'zod'],
-          'vendor-editors': ['@ckeditor/ckeditor5-react', 'react-quill'],
-          'vendor-utils': ['lodash', 'axios', 'dayjs'],
-          'vendor-fonts': ['./src/generated-fonts.css']
-        },
         // Reduce the number of chunks
         experimentalMinChunkSize: 10000,
         // Optimize chunk distribution
@@ -45,39 +34,33 @@ export default defineConfig({
           const facadeModuleId = chunkInfo.facadeModuleId ? chunkInfo.facadeModuleId.split('/') : [];
           const name = facadeModuleId[facadeModuleId.length - 2] || '[name]';
           return `js/${name}/[name].[hash].js`;
-        }
-      }
+        },
+      },
     },
     // Improve memory usage during build
     target: 'esnext',
     minify: 'esbuild',
     sourcemap: false,
     // Clean output directory before build
-    emptyOutDir: true
+    emptyOutDir: true,
   },
   // Optimize dev server
   server: {
     hmr: {
-      overlay: false
+      overlay: false,
     },
     watch: {
       usePolling: false,
-      ignored: ['**/node_modules/**', '**/dist/**']
-    }
+      ignored: ['**/node_modules/**', '**/dist/**'],
+    },
   },
   // Optimize dependency optimization
   optimizeDeps: {
-    include: [
-      'react',
-      'react-dom',
-      'lodash',
-      'antd',
-      '@mui/material'
-    ],
-    exclude: ['@fullcalendar/react']
+    include: ['react', 'react-dom', 'lodash', 'antd', '@mui/material'],
+    exclude: ['@fullcalendar/react'],
   },
   // Reduce memory usage during pre-bundling
   esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
-  }
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
 });
